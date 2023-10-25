9180762abf247aded7d4cbcb9fb6bba1a270c68e
Don Freddy <freddytamwo@gmail.com>
2023-10-25 13:35:08 +0100

    feat(lang): multi-languages support

---
+++ lib/app/app_config.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class TroonaLocales {
+  static final all = [
+    const Locale('en'),
+    const Locale('fr'),
+  ];
+}
---
+++ lib/app/troona_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:troona/core/config/routes/route.dart';
+import 'package:troona/core/config/routes/route_path.dart';
+import 'package:troona/core/config/themes/theme.dart';
+import 'package:troona/core/constants/constants.dart';
+
+class TroonaApp extends StatelessWidget {
+  const TroonaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Todo: Get it from user preferences
+    const accentColor = 0xffdd3f5d;
+
+    return NeumorphicApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      debugShowCheckedModeBanner: false,
+      locale: context.locale,
+      title: kAppName,
+      theme: lightTheme(accentColor),
+      darkTheme: darkTheme(accentColor),
+      initialRoute: RoutePath.loading,
+      onGenerateRoute: generateRoute,
+    );
+  }
+}
