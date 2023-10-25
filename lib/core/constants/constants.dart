6b618996392f32c976c51d0d7fa9539c158c9b03
Don Freddy <freddytamwo@gmail.com>
2023-10-25 13:31:09 +0100

    chore: update app name and package name

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,22 @@
+/// App name: `Troona`
+const String kAppName = 'Troona';
+
+/// Email: troonamusic@gmail.com
+const String kAppEmail = 'troonamusic@gmail.com';
+
+/// Font Family
+const String kFontFamily = 'Nunito';
+
+/// Radius size 10.0
+const double kRadius = 10.0;
+
+/// App Bar Height 55.0
+const double kAppBarHeight = 55.0;
+
+/// App content padding 16.0
+const double kAppContentPadding = 16.0;
+
+/// Mini player height 70.0
+const double kMiniPlayerHeight = 70.0;
+
+// •
---
+++ lib/loading_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
