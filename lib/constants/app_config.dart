8fccf3b6ad6d77e3614fed3da0c1d2c9cb38acdc
Don Freddy <freddytamwo@gmail.com>
2023-10-27 14:42:13 +0100

    chore: restructure app folders to use provider and add somme dependency

---
+++ lib/constants/app_config.dart
@@ -0,0 +1,28 @@
+/*
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona Music
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
+
+import 'dart:ui';
+
+class TroonaLocales {
+  static final all = [
+    const Locale('en'),
+    const Locale('fr'),
+  ];
+}
+
+const hiveBoxes = [
+  {'name': 'settings', 'limit': false},
+  {'name': 'stats', 'limit': false},
+  {'name': 'Favorite Songs', 'limit': false},
+  {'name': 'cache', 'limit': true},
+];
---
+++ lib/services/audio_handle.dart
@@ -0,0 +1,26 @@
+/*
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona Music
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
+
+import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
+
+abstract class TroonaAudioHandle {}
+
+class TroonaAudioHandleImpl extends BaseAudioHandler
+    with QueueHandler, SeekHandler
+    implements TroonaAudioHandle {
+
+
+  // Late initialization
+  late AudioPlayer? _player;
+}
