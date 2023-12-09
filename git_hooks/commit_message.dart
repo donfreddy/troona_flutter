2c6cc3c9e6d31905523111f9f4f950e82c6bfc39
Don Freddy <freddy.tamwo@neero.io>
2023-12-09 16:58:15 +0100

    chore: add header in commit_message.dart file

--- git_hooks/commit_message.dart
+++ git_hooks/commit_message.dart
@@ -1,3 +1,16 @@
+/*
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona Music
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
